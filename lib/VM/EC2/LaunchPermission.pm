186033c5dab0ee17af3b97e855a9e9e5f967dbd6
Lincoln Stein <lincoln.stein@gmail.com>
2011-07-21 14:00:01 -0400

    add ability to examine and modify image attributes; generalized mixed/underscore name handling

---
+++ lib/VM/EC2/LaunchPermission.pm
@@ -0,0 +1,67 @@
+package VM::EC2::LaunchPermission;
+
+=head1 NAME
+
+VM::EC2::Region - Object describing AMI launch permissions
+
+=head1 SYNOPSIS
+
+  use VM::EC2;
+
+  $ec2       = VM::EC2->new(...);
+  $image     = $ec2->describe_images('ami-12345');
+  @users     = $image->launchPermissions;
+  for (@users) {
+    $group = $_->group;
+    $user  = $_->userId;
+  }
+
+=head1 DESCRIPTION
+
+This object represents an Amazon machine image launch permission, and
+is return by VM::EC2::Image launchPermissions().
+
+=head1 METHODS
+
+These object methods are supported:
+
+ group      -- Name of a group with launch permissions. Only
+               valid value is "all"
+ userId     -- Name of a user with launch permissions.
+
+=head1 STRING OVERLOADING
+
+When used in a string context, this object will interpolate the
+userId. If the userId is blank, then interpolates as the group.
+
+=head1 SEE ALSO
+
+L<VM::EC2>
+L<VM::EC2::Generic>
+L<VM::EC2::Image>
+
+=head1 AUTHOR
+
+Lincoln Stein E<lt>lincoln.stein@gmail.comE<gt>.
+
+Copyright (c) 2011 Ontario Institute for Cancer Research
+
+This package and its accompanying libraries is free software; you can
+redistribute it and/or modify it under the terms of the GPL (either
+version 1, or at your option, any later version) or the Artistic
+License 2.0.  Refer to LICENSE for the full license text. In addition,
+please see DISCLAIMER.txt for disclaimers of warranty.
+
+=cut
+
+use strict;
+use base 'VM::EC2::Generic';
+
+sub primary_id {my $self = shift; $self->userId || $self->group };
+
+sub valid_fields {
+    my $self = shift;
+    return qw(group userId);
+}
+
+1;
