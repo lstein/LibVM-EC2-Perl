00409ac17bab44ab14cf2853584fae5ca10db0f7
Lincoln Stein <lincoln.stein@gmail.com>
2012-07-28 16:00:12 -0400

    ready changelog for 1.11

--- lib/VM/EC2.pm
+++ lib/VM/EC2.pm
@@ -354,7 +354,7 @@ use VM::EC2::Dispatch;
 use VM::EC2::Error;
 use Carp 'croak','carp';
 
-our $VERSION = '1.10';
+our $VERSION = '1.11';
 our $AUTOLOAD;
 our @CARP_NOT = qw(VM::EC2::Image    VM::EC2::Volume
                    VM::EC2::Snapshot VM::EC2::Instance
