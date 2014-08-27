10c14235cb3beb900ec224ea436a6571ce79c4b2
Lincoln Stein <lincoln.stein@gmail.com>
2014-08-26 23:54:45 -0400

    debugged spot_launch.pl script

--- bin/spot_launch.pl
+++ bin/spot_launch.pl
@@ -1,12 +1,106 @@
 #!/usr/bin/perl
 
+=head1 NAME
+
+ spot_launch.pl     Keep a series of spot instances running in the cheapest available zone.
+
+=head1 SYNOPSIS
+
+ % spot_launch.pl -verbose --tag=testlaunch --region=us-west-2 --image=ami-c04fc4f0 --count=2 --price=0.02 --security_group=web+ssh --key_name=my_default
+ Insufficient active spot requests (1 active, 0 pending). Requesting 1 new spot instances
+ Examining 30 day spot market for cheapest zone in which to run platform=Linux/UNIX, instance_type=t1.micro
+ Cheapest zone is us-west-2a; 30 day pricing for t1.micro = $ 0.003000/0.003100/0.00305215946843854 (min/max/mean)
+ 1  new spot instance requests launched in zone us-west-2a
+
+ % spot_launch.pl --status --tag=testlaunch --region=us-west-2
+ sir-03dppl2h: state=active status=fulfilled bid=0.020000 instanceId=i-2e500c25 instance_status=running created=2014-08-27T03:17:46.000Z public_dns=ec2-54-190-148-127.us-west-2.compute.amazonaws.com current_price=0.003100
+ sir-03dwpxa7: state=open status=pending-evaluation bid=0.020000
+
+=head1 DESCRIPTION
+
+This script surveys the 30 day pricing history in the EC2 region of
+your preference, finds the zone with the cheapest historical price for
+the instance type of your choice, and then creates the appropriate
+number of spot requests. You may run this script under cron to
+periodically check that your spot instances are running and launch new
+ones.
+
+The --status option prints out an easily-parsed report of the active
+spot requests and the instances running under them.
+
+To distinguish spot requests managed by this script from others,
+provide the --tag option. This will tag each of the spot requests with
+a distinguishing string.
+
+=head1 COMMAND-LINE ARGUMENTS
+
+  --access_key     <string>          Amazon access key (EC2_ACCESS_KEY environment var)
+  --secret_key     <string>          Amazon secret key (EC2_SECRET_KEY environment var)
+  --region         <string>          Desired region to run spot instances in (us-east-1)
+  --image          <ami-xxxx>        AMI to launch (no default)
+  --instance_type  <string>          Instance type to launch (t1.micro)
+  --key_name       <string>          SSH keyname for instances (optional, no default)
+  --security_group <string>          Security group name for instances (optional, no default)
+  --count          <integer>         Number of instances to launch (1)
+  --tag            <string>          Tag to attach to instances ("my_spot_instance")
+  --price          <float>           Maximum bid (no default)
+  --status                           Print status report on running instances
+  --verbose                          Verbose reporting
+
+You may use double-dashed long options in the form
+
+  --option=value
+
+single-dashed options in the form
+
+ -option value
+
+or abbreviate options to the smallest number of unique characters:
+
+ -o value
+
+Note that the --image and --price options are mandatory. You probably
+want to provide the --key_name option to allow SSH logins, and the
+--security_group option to control firewall rules.
+
+=head1 ENVIRONMENT VARIABLES
+
+The following environment variables are used if the corresponding
+options are not present:
+
+ EC2_ACCESS_KEY     your access key
+ EC2_SECRET_KEY     your secret key
+ EC2_URL            the desired region endpoint
+
+=head1 SEE ALSO
+
+L<VM::EC2>
+
+=head1 AUTHOR
+
+Lincoln Stein, lincoln.stein@gmail.com
+
+Copyright (c) 2014 Ontario Institute for Cancer Research This package
+and its accompanying libraries is free software; you can redistribute
+it and/or modify it under the terms of the GPL (either version 1, or
+at your option, any later version) or the Artistic License 2.0.  Refer
+to LICENSE for the full license text. In addition, please see
+DISCLAIMER.txt for disclaimers of warranty.
+
+=cut
+
+
 use strict;
-use VM::EC2 qw(:standard spot_instance);
-use Statistics::Descriptive;
-use Date::Parse;
 use Getopt::Long;
 use POSIX 'strftime';
 use Carp 'croak';
+use Memoize;
+memoize('get_current_price');
+
+load_module('Statistics::Descriptive');
+load_module('Date::Parse');
+load_module('VM::EC2');
+VM::EC2->import(':standard','spot_instance');
 
 my ($Access_key,$Secret_key,$Region,
     $Ami,$Instance_type,$Instance_count,$Tag,$Max_price,
@@ -40,13 +134,6 @@ Usage: $0 [options]
   -price          <float>           Maximum bid (no default)
   -status                           Print status report on running instances
   -verbose                          Verbose reporting
-
-This script requires the following Perl modules and their dependencies to be installed:
-  
-  1) VM::EC2
-  2) Date::Parse
-  3) Statistics::Descriptive
-
 USAGE
 
 # defaults
@@ -83,12 +170,14 @@ sub do_status_report {
     my @requests  = $ec2->describe_spot_instance_requests({'tag-key' => $tag});
 
     for my $r (@requests) {
+	next if $r->instance && $r->instance->status eq 'terminated';
 
-	print "$r:",' state=',$r->state,' status=',$r->status,' bid=',$r->spot_price,' fault=',$r->fault;
+	print "$r:",' state=',$r->state,' status=',$r->status,' bid=',$r->spot_price;
+	print ' fault=',$r->fault if $r->fault;
 	
-	if (my $i = $r->instanceId) {
-	    my $current_price = get_current_price($ec2,$r->launched_availability_zone,$r->productDescription,$r->instanceType);
-	    print " instanceId=$i",' created=',$r->create_time," public_dns=",$i->dnsName," current_price=$current_price";
+	if (my $i = $r->instance) {
+	    my $current_price = get_current_price($ec2,$r->launched_availability_zone,$r->productDescription,$i->instance_type);
+	    print " instanceId=$i",' instance_status=',$i->status,' created=',$r->create_time," public_dns=",$i->dnsName," current_price=$current_price";
 	}
 
 	print "\n";
@@ -103,16 +192,16 @@ sub do_instance_update {
 
     # get the spot instance requests that are e
     my @requests = $ec2->describe_spot_instance_requests({'tag-key' => $opt->{-tag},
-							  'state'   => ['open','fulfilled']
+							  'state'   => ['open','active']
 							 });
 
-    my $active  = grep {$_->state eq 'fulfilled'} @requests;
-    my $pending = grep {$_->state eq 'open'}      @requests;
+    my $active  = grep {$_->state eq 'active' && $_->instance->status eq 'running'} @requests;
+    my $pending = grep {$_->state eq 'open'}                                        @requests;
     $active  ||= 0;
     $pending ||= 0;
 
     # $count will be number of requests we need to launch
-    my $count = $opt->{-count} - @requests;
+    my $count = $opt->{-count} - ($active+$pending);
 
     # enough instances running or potentially running
     if ($count <= 0) { 
@@ -187,11 +276,11 @@ sub find_best_zone {
     
     return {
 	zone   => $sorted_zones[0],
-	min    => $zones{$sorted_zones[0]}->min,
-	max    => $zones{$sorted_zones[0]}->max,
-	mean   => $zones{$sorted_zones[0]}->mean,
-	median => $zones{$sorted_zones[0]}->median,
-	current=> $current_price{$sorted_zones[0]}{price}
+	min    => sprintf('%.4f',$zones{$sorted_zones[0]}->min),
+	max    => sprintf('%.4f',$zones{$sorted_zones[0]}->max),
+	mean   => sprintf('%.4f',$zones{$sorted_zones[0]}->mean),
+	median => sprintf('%.4f',$zones{$sorted_zones[0]}->median),
+	current=> sprintf('%.4f',$current_price{$sorted_zones[0]}{price})
     }
 }
 
@@ -201,7 +290,7 @@ sub get_current_price {
 						    -availability_zone    => $zone,
 						    -instance_type        => $type,
 						    -product_description  => $platform);
-    return $history[0]->spot_price;
+    return sprintf('%.4f',$history[0]->spot_price);
 }
 
 sub format_time {
@@ -214,3 +303,16 @@ sub logit {
     return unless $Verbose;
     print STDERR "@msg\n";
 }
+
+sub load_module {
+    my $module = shift;
+    return if eval "use $module; 1";
+    print STDERR "You are missing Perl module $module. Do you want to install it? [Yn]";
+    chomp(my $response = <STDIN>);
+    die "Aborted.\n" if $response && $response !~ /^[Yy]/;
+    eval "use CPAN";
+    CPAN::Shell->install($module);
+    eval "use $module";
+}
+
+
